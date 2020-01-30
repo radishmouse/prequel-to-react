60d7526f4d9a8736b4702dd9342ec31fbf6c0614
Chris Aquino <chrisaquino@radishmouse.local>
2020-01-30 12:05:39 -0500

    adds code examples for arg destructuring and obj spread

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// console.log('Hello? Does this work?');
+// console.log('Live reaload really works.');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// #1 Accept arguments instead
+// of using global variables.
+// function greet({name}) {
+    // console.log(typeof name);
+    // const {name} = whom;
+    // const name = whom.name;
+    // console.log(person);
+    // console.log(person2)
+    // Nooooo!
+    // Person is defined outside
+    // the function.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function greet({
+    name, 
+    address, 
+}) {    
+    // #2 Always return a value!
+    return `Hello, ${name} of ${address}`;
+}
+const person = {
+    name: 'Greta',
+    address: 'Alpha Centauri',
+    occupation: 'Hero'
+};
+function fromEarth(originalObject) {
+    // add an address property
+    // whose value is Earth
+    // originalObject.address = 'Earth'
+    // const theAddress = originalObject.address || 'Earth';
+    const modifedVersionOfObject = {
+        // defaults go before the 
+        // object spread operator (...)
+        address: 'Earth',
+        ...originalObject,  // object spread syntax
+        // address: theAddress,
+        // "take all the key value pairs from the original
+        // and sprinkle them right HERE."
+        // name: originalObject.name,
+        // occupation: originalObject.occupation,
+        // address: originalObject.address || 'Earth',
+        age: 'infinity',
+        // overwrites go after the
+        // the object spread operator (...)
+        name: 'Oakley'
+    }
+
+    // then return that object
+    return modifedVersionOfObject;
+}
+console.log(greet(fromEarth(person)));
+console.log(greet(person));
+// const person2 = {
+//     name: 'Travis'
+// };
+// console.log(greet(person2));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
